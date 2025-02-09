8200a169c9671dc53f60134fdaf938c17cf67685
Dhai <dhasaf@icloud.com>
2025-02-09 20:35:58 +0300

    Creating a new About Us page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   #get "pages/home"
   #Setting the home as the main or first page
   root "pages#home"
+  #Routing to About Us page
+  #get "pages/about"
+  get "about" => "pages#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
